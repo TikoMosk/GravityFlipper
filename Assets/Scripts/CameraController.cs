bb661cd0e4bc0298de69ff117854d1d348344d83
Tigran Ghukasian <urakhblog3@gmail.com>
2019-07-09 11:17:37 +0400

    Added camera rotation with touch drag

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Camera mainCam;
+    public float dragAmountToChangeCamera;
+    public float cameraSpeed;
+    Vector2 touchPos;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchPos = touch.position;
+            }
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 dragPos = touch.position;
+                if(Vector2.Distance(touchPos,dragPos) > dragAmountToChangeCamera)
+                {
+                    ChangeCamera(touchPos - dragPos);
+                }
+            }
+        }
+    }
+    private void ChangeCamera(Vector2 dragVector)
+    {
+        if(dragVector.x < 0)
+        {
+            mainCam.transform.RotateAround(Vector3.zero, Vector3.up,cameraSpeed * Time.deltaTime);
+        }
+        else if (dragVector.x > 0)
+        {
+            mainCam.transform.RotateAround(Vector3.zero, Vector3.down, cameraSpeed * Time.deltaTime);
+        }
     }
 }
