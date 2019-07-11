f989a6fa81aa1821fb9f60341f9eba7af8bf5ca3
Tigran Ghukasian <urakhblog3@gmail.com>
2019-07-11 11:30:45 +0400

    Created gravity changer by dragging

--- Assets/Scripts/GravityController.cs
+++ Assets/Scripts/GravityController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class GravityController : MonoBehaviour
 {
-    Vector2 touchStart;
-    Vector2 touchPos;
-    Vector2 touchDragVector;
+    public GameObject houseObject;
+    public float worldRotateTime;
+    private Vector2 touchStart;
+    private Vector2 touchPos;
+    private Vector2 touchDragVector;
+    
     private void Update()
     {
         if(Input.touchCount > 0)
@@ -19,10 +22,68 @@ public class GravityController : MonoBehaviour
             if(touch.phase == TouchPhase.Moved)
             {
                 touchPos = touch.position;
+                touchDragVector = touchPos - touchStart;
+                
             }
-            touchDragVector = touchPos - touchStart;
-            Debug.Log(touchDragVector);
+            if(touch.phase == TouchPhase.Ended)
+            {
+                FlipWorld(CheckDragDirection(touchDragVector));
+            }
+           
         }
+    }
+    private void FlipWorld(Vector3 direction)
+    {
+        if(direction != Vector3.zero)
+        {
+
+            StartCoroutine(RotateWorld(direction));
+        }
+    }
+    IEnumerator RotateWorld(Vector3 direction)
+    {
+        float angleRotated = 0;
+        float t = 0;
+        do
+        {
+            
+            t += Time.deltaTime;
+            float amountToRotate = Time.deltaTime * 90 / worldRotateTime;
+            if (t >= worldRotateTime)
+            {
+                amountToRotate = 90 - angleRotated;
+            }
+            houseObject.transform.RotateAround(houseObject.transform.position,direction.normalized, amountToRotate);
+            angleRotated += amountToRotate;
+
+            yield return null;
+        } while (t < worldRotateTime);
+    }
+    private Vector3 CheckDragDirection(Vector2 dragVector)
+    {
+        if(dragVector.y <= 0)
+        {
+            if(dragVector.x >= 0)
+            {
+                return new Vector3(0,0,1);
+            }
+            if (dragVector.x < 0)
+            {
+                return new Vector3(1,0,0);
+            }
+        }
+        else
+        {
+            if (dragVector.x >= 0)
+            {
+                return new Vector3(-1, 0,0 );
+            }
+            if (dragVector.x < 0)
+            {
+                return new Vector3(0, 0, -1);
+            }
+        }
+        return Vector2.zero;
     }
 
 }
