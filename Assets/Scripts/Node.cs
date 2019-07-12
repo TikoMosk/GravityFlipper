0c8199ed89bd724209d23fcd4216444867dbc02d
Hayko585 <47004031+Hayko588@users.noreply.github.com>
2019-07-12 11:34:51 +0400

    Node, NodeControl

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private int pointX, pointY, pointZ;
+    public Node(GameObject obj, int x, int y, int z)
+	{
+        nodeObject = obj;
+		this.pointX = x;
+		this.pointY = y;
+		this.pointZ = z;
+	}
+
+	public GameObject nodeObject;
+	
+    internal Vector3 GetNodePosition()
+    {
+        return new Vector3(pointX, pointY, pointZ);
+    }
+
+    public void InstantiateNode()
+    {
+        Instantiate(nodeObject, GetNodePosition(), Quaternion.identity);
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("MouseDown");
+    }
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NodeControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeControl : MonoBehaviour
+{
+    public GameObject playerObject;
+    public GameObject obj;
+    //ToDo //public GameObject staticLaserObject;
+
+    void Start()
+    {
+        Node node = new Node(obj, 0, 0, 0);
+        Debug.Log("start");
+        node.InstantiateNode();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
