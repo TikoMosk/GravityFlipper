71f53aa1a8dece74c70728ee18b5586d7d1699a1
Hayko585 <47004031+Hayko588@users.noreply.github.com>
2019-07-26 15:53:01 +0400

    Revert "////"
    This reverts commit 15d999d8b273047b32f5ed45920ffe72313715a9.
    
    # Conflicts:
    #	.vs/GravityFlipper/xs/project-cache/Assembly-CSharp-Debug.json
    #	Assets/Scripts/LevelDesigner.meta

--- Assets/Scripts/PlayerEventController.cs
+++ Assets/Scripts/PlayerEventController.cs
@@ -4,36 +4,10 @@ using UnityEngine;
 
 public class PlayerEventController : MonoBehaviour
 {
-<<<<<<< HEAD
-    private Node currentNode;
-
-    void Start()
-    {
-        
-    }
-=======
-	private bool isEnable;
-    private void Start()
-    {
-		TurnEventSystem.currentInstance.RegisterOnEvent(Foo);
-        TurnEventSystem.currentInstance.RemoveFromEvent(Foo);
-    }
-
-    private void Foo()
-    {
-		if (!isEnable)
-			transform.localScale = transform.localScale * 2;
-		else
-			transform.localScale = transform.localScale / 2;
-
-		isEnable = !isEnable;
-	}
-
     private void OnMouseDown()
     {
-		TurnEventSystem.currentInstance.NextTurn();
-	}
->>>>>>> parent of b4c594b... //test// laser, enemy
+        TurnEventSystem.currentInstance.NextTurn();
+    }
 
     void Update()
     {
