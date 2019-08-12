bb0b2f986f3f1a7a3d501dc7200a3f176b3c9c54
Hayko585 <47004031+Hayko588@users.noreply.github.com>
2019-08-12 15:52:17 +0400

    last

--- Assets/Scripts/TurnSystem/PursuitScript.cs
+++ Assets/Scripts/TurnSystem/PursuitScript.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PursuitScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3[] directions;
+    private void Start()
     {
-        
+        EventController.currentInstance.Register(Check);
+
+        directions = new Vector3[6];
+        directions[0] = new Vector3(1, 0, 0);
+        directions[1] = new Vector3(0, 1, 0);
+        directions[2] = new Vector3(0, 0, 1);
+        directions[3] = new Vector3(-1, 0, 0);
+        directions[4] = new Vector3(0, -1, 0);
+        directions[5] = new Vector3(0, 0, -1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartPursuit()
     {
-        
+        GetComponent<PatrolScript>().enabled = false;
+        Debug.Log("StartPusr");
+    }
+
+    public void EndPursuit()
+    {
+        GetComponent<PatrolScript>().enabled = true;
+    }
+
+    private void Check()
+    {
+        Vector3 vector = transform.position;
+        foreach (var step in directions)
+        {
+            vector += step;
+            if (GameController.Game.CurrentLevel.GetNode((int)vector.x, (int)vector.y, (int)vector.z).Id == 1)
+            {
+                StartPursuit();
+            }
+        }
     }
 }
