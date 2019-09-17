f3aa4a0c72ad752e0dc8276481750520a7856793
Narek <nareksaghiyan@gmail.com>
2019-09-17 18:57:16 +0400

    Loading Bar Added

---
+++ Assets/Scripts/Menu/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadLevel(int sceneIndex)
+    {
+        LoadAsynchronously(sceneIndex);
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(sceneIndex);
+
+
+            yield return null;
+        }
+    }
+}
