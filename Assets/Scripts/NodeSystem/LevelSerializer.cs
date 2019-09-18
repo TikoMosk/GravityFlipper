d44b33b03b3fcda7a831465e0e2a5bcc018d1a90
Hayko585 <47004031+Hayko588@users.noreply.github.com>
2019-09-18 19:16:36 +0400

    mysql db connection etc

--- Assets/Scripts/NodeSystem/LevelSerializer.cs
+++ Assets/Scripts/NodeSystem/LevelSerializer.cs
@@ -5,43 +5,54 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class LevelSerializer : MonoBehaviour {
+public class LevelSerializer : MonoBehaviour
+{
     private string path;
     Level level;
 
-    IEnumerator GetRequest(string url) {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(url)) {
+    IEnumerator GetRequest(string url)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
             // Request and wait for the desired page.
             yield return webRequest.SendWebRequest();
 
 
-            if (webRequest.isNetworkError) {
+            if (webRequest.isNetworkError)
+            {
                 Debug.Log("error");
             }
-            else {
-                Debug.Log(webRequest.downloadHandler.text.Replace("\\",""));
+            else
+            {
+                Debug.Log(webRequest.downloadHandler.text.Replace("\\", ""));
                 LevelData levelData = JsonUtility.FromJson<LevelData>(webRequest.downloadHandler.text);
                 level = DeserializeLevel(levelData);
                 //Debug.Log(webRequest.downloadHandler.text);
             }
         }
     }
-    public void LoadDevLevel(int levelNumber) {
-        string serverURL = "localhost:5000/levels/";
+    
+    public void LoadDevLevel(int levelNumber)
+    {
+        string serverURL = "localhost:3000/";
         StartCoroutine(GetRequest(serverURL + levelNumber.ToString()));
     }
-    public Level LoadLevelLocal(string path) {
+
+    public Level LoadLevelLocal(string path)
+    {
         string result = null;
 
-        string filePath = Path.Combine(Application.streamingAssetsPath ,path);
+        string filePath = Path.Combine(Application.streamingAssetsPath, path);
 
-        if (Application.platform == RuntimePlatform.Android) {
+        if (Application.platform == RuntimePlatform.Android)
+        {
             WWW reader = new WWW(filePath);
-           
-            while (!reader.isDone) {}
+
+            while (!reader.isDone) { }
             result = reader.text;
         }
-        else {
+        else
+        {
 
             result = File.ReadAllText(filePath);
 
@@ -50,38 +61,51 @@ public class LevelSerializer : MonoBehaviour {
         Level level = DeserializeLevel(levelData);
         return level;
     }
-    public Level LoadLevelFromServer(string path) {
-       //TODO:
-       return null;
+
+    public Level LoadLevelFromServer(int levelID)
+    {
+        string savePath = @"/Users/hp/gravityflipper/Assets/StreamingAssets/";
+
+        return null;
     }
 
-    public void SaveLevelLocal(string path,Level level) {
-
+    public void SaveLevelLocal(string path, Level level)
+    {
         string filePath = Path.Combine(Application.streamingAssetsPath, path);
         LevelData levelData = SerializeLevel(level);
         string str = JsonUtility.ToJson(levelData, true);
-        if (Application.platform == RuntimePlatform.Android) {
+        if (Application.platform == RuntimePlatform.Android)
+        {
             WWW reader = new WWW(filePath);
-            while (!reader.isDone) {}
+            while (!reader.isDone) { }
             File.WriteAllText(filePath, str);
         }
-        else {
+        else
+        {
             File.WriteAllText(filePath, str);
 
         }
 
     }
-    private LevelData SerializeLevel(Level level) {
+
+    private LevelData SerializeLevel(Level level)
+    {
         LevelData levelData = new LevelData(level.Width, level.Height, level.Length);
-        for (int x = 0; x < level.Width; x++) {
-            for (int y = 0; y < level.Height; y++) {
-                for (int z = 0; z < level.Length; z++) {
-                    if (level.NodeMap[x, y, z].Id != 0) {
+        for (int x = 0; x < level.Width; x++)
+        {
+            for (int y = 0; y < level.Height; y++)
+            {
+                for (int z = 0; z < level.Length; z++)
+                {
+                    if (level.NodeMap[x, y, z].Id != 0)
+                    {
                         NodeData nodeData = new NodeData(level.NodeMap[x, y, z]);
                         levelData.nodeDataList.Add(nodeData);
                     }
-                    if (level.NodeMap[x, y, z].NodeMember != null) {
-                        if (level.NodeMap[x, y, z].NodeMember.Id != 0) {
+                    if (level.NodeMap[x, y, z].NodeMember != null)
+                    {
+                        if (level.NodeMap[x, y, z].NodeMember.Id != 0)
+                        {
                             NodeMemberData nodeMemberData = new NodeMemberData(level.NodeMap[x, y, z].NodeMember, x, y, z);
                             levelData.nodeMemberDataList.Add(nodeMemberData);
                         }
@@ -93,33 +117,40 @@ public class LevelSerializer : MonoBehaviour {
         }
         return levelData;
     }
-    private Level DeserializeLevel(LevelData levelData) {
+
+    private Level DeserializeLevel(LevelData levelData)
+    {
         Level level = new Level(levelData.width, levelData.height, levelData.length);
         level.InitializeLevel();
         int x, y, z;
         Node.Direction facing = Node.Direction.FORWARD;
         Node.Direction upDirection = Node.Direction.UP;
-        for (int i = 0; i < levelData.nodeDataList.Count; i++) {
+
+        for (int i = 0; i < levelData.nodeDataList.Count; i++)
+        {
             NodeData nodeData = levelData.nodeDataList[i];
             x = nodeData.x;
             y = nodeData.y;
             z = nodeData.z;
-            if (nodeData.facing != "" && nodeData.upDirection != "") {
+            if (nodeData.facing != "" && nodeData.upDirection != "")
+            {
                 facing = (Node.Direction)int.Parse(nodeData.facing);
                 upDirection = (Node.Direction)int.Parse(nodeData.upDirection);
             }
-            level.SetNode(x, y, z, nodeData.id,facing,upDirection);
+            level.SetNode(x, y, z, nodeData.id, facing, upDirection);
         }
-        for (int i = 0; i < levelData.nodeMemberDataList.Count; i++) {
+        for (int i = 0; i < levelData.nodeMemberDataList.Count; i++)
+        {
             NodeMemberData nodeMemberData = levelData.nodeMemberDataList[i];
             x = nodeMemberData.x;
             y = nodeMemberData.y;
             z = nodeMemberData.z;
-            if(nodeMemberData.facing != "" && nodeMemberData.upDirection != "") {
+            if (nodeMemberData.facing != "" && nodeMemberData.upDirection != "")
+            {
                 facing = (Node.Direction)int.Parse(nodeMemberData.facing);
                 upDirection = (Node.Direction)int.Parse(nodeMemberData.upDirection);
             }
-            
+
             level.AddNodeMember(x, y, z, nodeMemberData.id, facing, upDirection);
         }
 
@@ -129,28 +160,33 @@ public class LevelSerializer : MonoBehaviour {
 
 }
 [System.Serializable]
-public class Data {
+public class Data
+{
 
 }
 [System.Serializable]
-public class NodeData {
+public class NodeData
+{
     public int x;
     public int y;
     public int z;
     public int id;
     public string facing;
     public string upDirection;
-    public NodeData() {
+    public NodeData()
+    {
 
     }
-    public NodeData(Node n) {
+    public NodeData(Node n)
+    {
         x = n.X;
         y = n.Y;
         z = n.Z;
         id = n.Id;
         //TODO: DIRECTIONS
         //direction = n.direction;
-        if (!(n.Facing == Node.Direction.FORWARD && n.UpDirection == Node.Direction.UP)) {
+        if (!(n.Facing == Node.Direction.FORWARD && n.UpDirection == Node.Direction.UP))
+        {
             facing = ((int)n.Facing).ToString();
             upDirection = ((int)n.UpDirection).ToString();
         }
@@ -158,17 +194,20 @@ public class NodeData {
 
 }
 [System.Serializable]
-public class NodeMemberData {
+public class NodeMemberData
+{
     public int x;
     public int y;
     public int z;
     public int id;
     public string facing;
     public string upDirection;
-    public NodeMemberData() {
+    public NodeMemberData()
+    {
 
     }
-    public NodeMemberData(NodeMember nm, int x, int y, int z) {
+    public NodeMemberData(NodeMember nm, int x, int y, int z)
+    {
         this.x = x;
         this.y = y;
         this.z = z;
@@ -178,14 +217,17 @@ public class NodeMemberData {
     }
 }
 [System.Serializable]
-public class LevelData {
+public class LevelData
+{
     public int width = 0;
     public int height = 0;
     public int length = 0;
-    public LevelData() {
+    public LevelData()
+    {
 
     }
-    public LevelData(int width, int height, int length) {
+    public LevelData(int width, int height, int length)
+    {
         this.width = width;
         this.height = height;
         this.length = length;
