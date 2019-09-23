109695e50e2f2f239ec54e7e687ccdeb6b2e66f9
Hayko585 <47004031+Hayko588@users.noreply.github.com>
2019-09-23 13:27:17 +0400

    //

--- Assets/Scripts/NodeSystem/LevelSerializer.cs
+++ Assets/Scripts/NodeSystem/LevelSerializer.cs
@@ -152,17 +152,17 @@ public class LevelSerializer : MonoBehaviour
 
     private void OnGUI()
     {
-        usr = GUI.TextField(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 20), usr, 12);
-
-        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 20), "Username: " + usr))
-        {
-            UploadNewUser(usr);
-        }
-
-        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 25, 200, 20), "UpdateUserLevels"))
-        {
-            UnlockLevels(17);
-        }
+        //usr = GUI.TextField(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 20), usr, 12);
+        //
+        //if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 20), "Username: " + usr))
+        //{
+        //    UploadNewUser(usr);
+        //}
+        //
+        //if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 50, 200, 20), "UpdateUserLevels"))
+        //{
+        //    UnlockLevels(15);
+        //}
     }
 
 
