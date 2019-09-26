f7c2d65fbead8276869aae1a6f0ba3ccb1edb188
Hayko585 <47004031+Hayko588@users.noreply.github.com>
2019-09-26 14:27:40 +0400

    //

--- Assets/Scripts/NodeSystem/NodeToggler.cs
+++ Assets/Scripts/NodeSystem/NodeToggler.cs
@@ -2,65 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NodeToggler : MonoBehaviour {
+public class NodeToggler : MonoBehaviour
+{
     public GameObject child;
     private Node connectedNode;
     private NodeMember connectedNodeMember;
     private bool pushed;
     private Color32 connectionColor = Color.clear;
     public Color32 ConnectionColor { get => connectionColor; set => connectionColor = value; }
-    public void ConnectNode(Node n) {
+    public void ConnectNode(Node n)
+    {
         connectedNode = null;
         connectedNodeMember = null;
         GameController.Game.CurrentLevel.RemoveNodeConnection(this);
-        if (n.NodeGraphic != null) {
-            if (n.NodeGraphic.GetComponent<NodeToggleReceiver>() != null) {
+        if (n.NodeGraphic != null)
+        {
+            if (n.NodeGraphic.GetComponent<NodeToggleReceiver>() != null)
+            {
                 connectedNode = n;
             }
         }
-        else if (n.NodeMember != null) {
-            if (n.NodeMember.NodeObjectGraphic.GetComponent<NodeToggleReceiver>() != null) {
+        else if (n.NodeMember != null)
+        {
+            if (n.NodeMember.NodeObjectGraphic.GetComponent<NodeToggleReceiver>() != null)
+            {
                 connectedNodeMember = n.NodeMember;
             }
         }
-        else {
+        else
+        {
             return;
         }
         GameController.Game.CurrentLevel.AddNodeConnection(this);
     }
-    public Vector3 GetConnectNodePosition() {
-        if (connectedNode != null) {
+    public Vector3 GetConnectNodePosition()
+    {
+        if (connectedNode != null)
+        {
             return connectedNode.GetPosition();
         }
-        else if (connectedNodeMember != null) {
+        else if (connectedNodeMember != null)
+        {
             return connectedNodeMember.LocationNode.GetPosition();
         }
-        else {
-            return new Vector3(-1,-1,-1);
+        else
+        {
+            return new Vector3(-1, -1, -1);
         }
     }
-    public Vector3 GetPos() {
+
+    public Vector3 GetPos()
+    {
         return transform.position;
     }
-    public void Toggle() {
-        if(GameController.Game.SmoothGraphics.AnimationCount == 0) {
+
+    public void Toggle()
+    {
+        if (GameController.Game.SmoothGraphics.AnimationCount == 0)
+        {
             pushed = !pushed;
             child.GetComponent<Animator>().SetBool("Enabled", pushed);
-            if (connectedNode != null) {
+            if (connectedNode != null)
+            {
                 connectedNode.NodeGraphic.GetComponent<NodeToggleReceiver>().Trigger();
             }
-            else if (connectedNodeMember != null) {
+            else if (connectedNodeMember != null)
+            {
                 connectedNodeMember.NodeObjectGraphic.GetComponent<NodeToggleReceiver>().Trigger();
             }
         }
-        
+
 
     }
-    public bool CheckIfSameConnectedNode(Node n) {
-        if (connectedNode == n) {
+
+    public bool CheckIfSameConnectedNode(Node n)
+    {
+        if (connectedNode == n)
+        {
             return false;
         }
-        else {
+        else
+        {
             return true;
         }
     }
